bf227972759441c1a4b9bca155dc3915b250f95e
AgentO3 <ozanzal@gmail.com>
2020-06-11 06:40:30 -0400

    Adds frame and description card

--- _artworks/back-to-back.markdown
+++ _artworks/back-to-back.markdown
@@ -1,7 +1,7 @@
 ---
 image: back-to-back.jpg
 title:  Back to Back
-created: September 2013
+created: 2013
 artist: John Zanzal
 room: Gemstones
 link: https://www.facebook.com/photo.php?fbid=10153301575160195&set=pb.846910194.-2207520000..&type=3&theater
--- assets/js/image-size.js
+++ assets/js/image-size.js
@@ -3,12 +3,18 @@ AFRAME.registerComponent('image-size', {
         let el = this.el
         let srcId = el.getAttribute('src')
         let img = document.getElementById(srcId.replace('#', ''))
+        let frame = el.querySelector('.frame')
         let w = img.naturalWidth
         let h = img.naturalHeight
         let ratio = w / h
         let newW = 1
         let newH = newW / ratio
+        let padding = 0.25
         el.setAttribute('height', newH)
         el.setAttribute('width', newW)
+        if (frame) {
+            frame.setAttribute('height', newH + padding)
+            frame.setAttribute('depth', newW + padding)
+        }
     }
 });
